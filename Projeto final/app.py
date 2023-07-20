e8448456148a6c53fe403eef2cd6ed4b06ee7084
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2023-07-20 20:16:18 -0300

    Update app.py

--- Projeto final/app.py
+++ Projeto final/app.py
@@ -15,10 +15,15 @@ def cadastrar_fornecedor(cadastro_fornecedores):
 =======
     qtd_numero_telefone = 11
     while not telefone_fornecedor or len(telefone_fornecedor) != qtd_numero_telefone or not telefone_fornecedor.isdigit():
+<<<<<<< HEAD
+        telefone_fornecedor = input("Digite o telefone do fornecedor (11 dígitos): ")
+        
+=======
         telefone_fornecedor = input("Digite o telefone do fornecedor (até, qtd_numero_telefone, dígitos): ")
 >>>>>>> 80643b7c2b0d65619d848f8a12626e4bb33dd901
+>>>>>>> b9b791a4f6ec1e828616c31296e057da536404a6
         if len(telefone_fornecedor) != qtd_numero_telefone or not telefone_fornecedor.isdigit():
-            print("Telefone inválido. Digite até", qtd_numero_telefone, "números.")
+            print("Telefone inválido. Digite no mínimo", qtd_numero_telefone, "números.")
     
     email_fornecedor = None
     while not email_fornecedor or "@" not in email_fornecedor or "." not in email_fornecedor:
@@ -78,6 +83,7 @@ def logar():
             print("Senha ou usuário incorretos. Você ainda tem", 2 - tentativa, "tentativa(s)")
             usuario = input("Digite o usuário: ")
             senha = input("Digite a senha: ")
+            
         tentativa = tentativa + 1
 
 def menu():
@@ -104,5 +110,5 @@ def menu():
             print("Opção inválida. Digite novamente.")
 
 if __name__ == "__main__":
-    logar()
-    menu()
+    if logar():
+        menu()
